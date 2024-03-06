ee7fd1e9fa977eeb09016ee893762345bc1b2690
SGrashch <sergey.grashchenkov@ypmn.ru>
2024-03-06 13:23:30 +0300

    V1.0

--- Examples_requests/importLibs.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from datetime import datetime
-import hashlib
-import hmac
-import requests
-from random import randint
-import json
--- ypmn_classes_payout.py
+++ ypmn_classes_payout.py
@@ -1,8 +1,4 @@
 from typing import List
-from datetime import datetime
-import hashlib
-import hmac
-import requests
 from random import randint
 import json
 
